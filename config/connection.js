00fe5b024e84a492b429846c8e118d0dc73becde
efrencav <efren@abstract-creative.com>
2018-11-18 20:24:33 -0600

    massive update

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+// const connection = mysql.createConnection(process.env.JAWSDB_URL);
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ controllers/burgersController.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const burger = require("../models/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    const hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function (result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  burger.update({
+      devoured: req.body.devoured
+    },
+    req.params.id,
+    function (result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } 
+        res.status(200).end();
+      }
+    );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
